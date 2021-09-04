78858d3b28d4f947684c7f144ea673dfd6079bcf
NagaYashaswiniDevalla <nagayashaswinidevalla@gmail.com>
2021-09-04 08:58:28 +0530

    adding files

---
+++ Subscriber.java
@@ -0,0 +1,19 @@
+Package com.Event;
+Public class Subscriber
+{
+	Private String name ;
+	Private Channel = new Channel ();
+	Public Subcriber(String name)
+	{
+		Super()
+		This.name=name;
+	}
+	Public void update ()
+	{
+		System.out.println(“Video uploaded “);
+	}
+	Public void subscribeChannel(Channel Ch )
+	{
+		Channel Ch ;
+	}
+}
---
+++ Youtube.java
@@ -0,0 +1,21 @@
+Package com.Event;
+Public class Youtube 
+{
+	Public static void main (String [] Atgs )
+	{
+		Channel Event = new Channel();
+		Subscriber s1 = new Subcriber(“Yash”);
+		Subscriber s2 = new Subcriber(“Harika”);
+		Subscriber s3 = new Subcriber(“Thanu”);
+		Subscriber s4 = new Subcriber(“Sony”);
+		Event.Subscribe(s1);
+		Event.Subscribe(s2);
+		Event.Subscribe(s3);
+		Event.Subscribe(s4);
+		S1.subscribeChannel(Event);
+		S2.subscribeChannel(Event);
+		S3.subscribeChannel(Event);
+		S4.subscribeChannel(Event);
+		Telusko.upload(“Recycling the Waste”);
+	}
+}
